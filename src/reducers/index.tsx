1dabe9fd426eb6f7d150caf0167578e6c1956d04
krzysiek <krzysztof.malec@ignited.pl>
2020-02-29 14:33:12 +0100

    Add reducers, store, interfaces, Chanhe App and index

---
+++ src/reducers/index.tsx
@@ -0,0 +1,10 @@
+import { Reducer } from 'redux';
+import { AppStateinterface } from '../interfaces';
+
+const initialState: AppStateinterface = {
+  sample: '',
+};
+
+const rootReducer: Reducer = (state = initialState, action) => state;
+
+export default rootReducer;
