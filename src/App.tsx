88fd6aee3d797f12dd477c9fb0ddbd2b171dbc74
krzysiek <krzysztof.malec@ignited.pl>
2020-02-29 09:34:22 +0100

    Check lint staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-const App: React.FC = () => <div>APP init</div>;
+const App: React.FC = () => {
+  console.log("works");
+
+  return <div>APP init</div>;
+};
 
 export default App;
