36d9f1b187ad5f2928aa603d69d79878302c649e
krzysiek <krzysztof.malec@ignited.pl>
2020-02-29 09:34:43 +0100

    Remove console .log

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-const App: React.FC = () => {
-  console.log("works");
-
-  return <div>APP init</div>;
-};
+const App: React.FC = () => <div>APP init</div>;
 
 export default App;
