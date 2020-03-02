2f4de03112e5b5f944baeadc408b42ad38480bde
krzysiek <krzysztof.malec@ignited.pl>
2020-03-02 19:17:21 +0100

    Add button component [WIP]

---
+++ src/components/search-list/search-list-button/SearchListButton.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SearchListButton: React.FC = () => <button className="btn btn-primary btn-sm c-search-list-button">Add</button>;
+
+export default SearchListButton;
