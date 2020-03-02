58bde7957af885deab6bcc05bc3a631dddfc333d
krzysiek <krzysztof.malec@ignited.pl>
2020-03-02 15:13:43 +0100

    Implement components

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import store from './store';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Form from './components/form/Form';
+import SearchList from './components/search-list/SearchList';
 
 const App: React.FC = () => (
   <Provider store={store}>
     <Header />
     <Form />
+    <SearchList />
     <Footer />
   </Provider>
 );
--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -1,3 +1,12 @@
+export interface SearchItemInterfaceDeeper {
+  title?: string;
+  url?: string;
+}
+
 export interface AppStateinterface {
   queryResults: object[];
+}
+
+export interface SearchItemInterface {
+  prop: SearchItemInterfaceDeeper;
 }
