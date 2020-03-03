20e803b76083cee7e511a140cef96d6b7b46537c
krzysiek <krzysztof.malec@ignited.pl>
2020-03-03 07:13:57 +0100

    Add add to playlist button logic

--- src/components/search-list/search-list-button/SearchListButton.tsx
+++ src/components/search-list/search-list-button/SearchListButton.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
-const SearchListButton: React.FC = () => <button className="btn btn-primary btn-sm c-search-list-button">Add</button>;
+import { OneKeyObjectInterface } from '../../../interfaces';
+import { addToPlaylist } from '../../../actions';
+
+const SearchListButton: React.FC<OneKeyObjectInterface> = (videoDetails: OneKeyObjectInterface) => {
+  const dispatch = useDispatch();
+
+  const handleClick = (): object => dispatch(addToPlaylist(videoDetails));
+
+  return (
+    <button className="btn btn-primary btn-sm c-search-list-button" onClick={handleClick}>
+      Add
+    </button>
+  );
+};
 
 export default SearchListButton;
