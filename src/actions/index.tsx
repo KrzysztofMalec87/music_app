1bee7c86b8008fdb084045ff3f61ba515b0388ca
krzysiek <krzysztof.malec@ignited.pl>
2020-03-26 11:35:45 +0100

    Add new actions

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -7,3 +7,13 @@ export const addToPlaylist = (playlistItem: object): object => ({
   type: 'ADD_TO_PLAYLIST',
   payload: playlistItem,
 });
+
+export const removeFromPlaylist = (playlistItem: object): object => ({
+  type: 'REMOVE_FROM_PLAYLIST',
+  payload: playlistItem,
+});
+
+export const setCurrentPlayed = (index: number): object => ({
+  type: 'SET_CURRENT_PLAYED',
+  payload: index,
+});
